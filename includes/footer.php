23349700f719a22d5bf37f6e73f12eaf513af552
Mihaly Varga <mihaly.varga@etu.unice.fr>
2017-11-14 19:05:44 +0100

    dashboard update

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+ <!-- jQuery -->
+    <script src="assests/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="assests/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="assests/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/dashboard.js"></script>
+
+</body>
+
+</html>
